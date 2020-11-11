df6edeb70b82474a577ed5bee20f224e5da06ad7
jaroon09et <68173312+jaroon09et@users.noreply.github.com>
2020-11-11 13:32:03 +0700

    Dbot9.py

---
+++ Dbot9.py
@@ -0,0 +1,317 @@
+ -*- coding: utf-8 -*-
+from MGET import *
+from akad.ttypes import *
+from multiprocessing import Pool, Process
+from datetime import datetime
+from time import sleep
+from bs4 import BeautifulSoup
+from humanfriendly import format_timespan, format_size, format_number, format_length
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz, urllib.request, urllib.parse, urllib.error, urllib.parse,antolib,subprocess,unicodedata,GACSender
+from gtts import gTTS
+from googletrans import Translator
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from linebot.exceptions import LineBotApiError
+
+#==============================================================================#
+botStart = time.time()
+#==============================================================================#
+line = LINE()
+line.https.driver = LINE(' r '  "https://manager.line.biz/account/@895eqofk/autoresponse/edit/26263496")
+line = LINE('swdBcr9AX17OH9I9qUrHDkKBu4jYHXrvlPzzFR06CXkxsmr6L6VN5L0gyu7XCuLgNZSOE3hlyOM=')
+line.log("Auth Token : " + str(line.authToken))
+line.log("Timeline Token : " + str(line.tl.channelAccessToken))
+line.bot.api=<jp.naver.line.android">	Download LINE
+botStart=([o]line.https.driver>member_Joineg_Group):
+print ("Login Succes")
+
+lineMID = line.profile.mid
+lineProfile = line.getProfile()
+lineSettings = line.getSettings()
+
+oepoll = OEPoll(line)
+#call = Call(line)
+readOpen = codecs.open("read.json","r","utf-8")
+settingsOpen = codecs.open("temp.json","r","utf-8")
+read = json.load(readOpen)
+settings = json.load(settingsOpen)
+Rfu = [line]
+Exc = [line]
+lineMID = line.getProfile().mid
+bot1 = line.getProfile().mid
+RfuBot=[lineMID]
+Family=["45fc9e7d89c28dcc9cd54df4d04dcdb0",lineMID]
+admin=['45fc9e7d89c28dcc9cd54df4d04dcdb0',lineMID]
+RfuFamily = RfuBot + Family
+
+protectname = []
+protecturl = []
+protection = []
+autocancel = {}
+autoinvite = []
+autoleaveroom = []
+targets = []
+#==============================================================================#
+msg_dict = {}
+
+settings = {
+    "autoBlock": False,
+    "autoAdd": True,
+    "autoJoin": True,
+    'autoCancel':{"on":True,"members":10},	
+    "autoLeave": True,
+    "autoRead": True,
+    "leaveRoom": False,
+    "detectMention": True,
+    "checkSticker": False,
+    "checkContact": False,
+    "checkPost": False,
+    "kickMention": False,
+    "potoMention": True,
+    "delayMention": False,
+    "lang":"JP",
+    "Wc": True,
+    "Lv": True,
+    "Nk": True,
+    "Api": True,
+    "Aip": True,
+    "blacklist":{},
+    "winvite": False,
+    "wblacklist": False,
+    "dblacklist": False,
+    "gift":False,
+    "likeOn":True,
+    "timeline":True,
+    "commentOn":True,
+    "commentBlack":{},
+    "wblack": False,
+    "dblack": False,
+    "clock": False,
+    "cName":"",
+    "cNames":"",
+    "changeGroupPicture": [],
+    "changePictureProfile":False,
+    "unsendMessage": False,
+    "autoJoinTicket": False,
+    "welcome":"[https://line.me/ti/p/~@895eqofk]",
+    "kick":"เอาหวะใจแม่งได้หวะ",
+    "bye":"นายทำดีแล้วเพื่อน ลาก่อน",
+    "Respontag":"💗",
+    "eror":"",
+    "spam":{},
+    "invite": {},
+    "winvite": False,
+    "pnharfbot": {},
+    "pname": {},
+    "pro_name": {},
+    "message1":"",
+    "messageadd":"",
+    "userAgent": [
+        "Mozilla/5.0 (X11; U; Linux i586; de; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (X11; U; Linux amd64; rv:5.0) Gecko/20100101 Firefox/5.0 (Debian)",
+        "Mozilla/5.0 (X11; U; Linux amd64; en-US; rv:5.0) Gecko/20110619 Firefox/5.0",
+        "Mozilla/5.0 (X11; Linux) Gecko Firefox/5.0",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:5.0) Gecko/20100101 Firefox/5.0 FirePHP/0.5",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:5.0) Gecko/20100101 Firefox/5.0 Firefox/5.0",
+        "Mozilla/5.0 (X11; Linux x86_64) Gecko Firefox/5.0",
+        "Mozilla/5.0 (X11; Linux ppc; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (X11; Linux AMD64) Gecko Firefox/5.0",
+        "Mozilla/5.0 (X11; FreeBSD amd64; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:5.0) Gecko/20110619 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 6.1; rv:6.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 6.1.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 5.2; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 5.1; U; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 5.1; rv:2.0.1) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 5.0; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0",
+        "Mozilla/5.0 (Windows NT 5.0; rv:5.0) Gecko/20100101 Firefox/5.0"
+    ],
+    "mimic": {
+        "copy": False,
+        "status": False,
+        "target": {}
+    }
+}
+
+RfuProtect = {
+    "protect": False,
+    "cancelprotect": False,
+    "inviteprotect": False,
+    "linkprotect": False,
+    "Protectguest": False,
+    "Protectjoin": False,
+    "autoBlock": True,
+}
+
+Setmain = {
+    "foto": {},
+}
+
+read = {
+    "readPoint": {},
+    "readMember": {},
+    "readTime": {},
+    "setTime":{},
+    "ROM": {}
+}
+
+myProfile = {
+	"displayName": "",
+	"statusMessage": "",
+	"pictureStatus": ""
+}
+
+mimic = {
+    "copy":False,
+    "copy2":False,
+    "status":False,
+    "target":{}
+    }
+    
+RfuCctv={
+    "cyduk":{},
+    "point":{},
+    "sidermem":{}
+}
+
+rfuSet = {
+    'setTime':{},
+    'ricoinvite':{},
+    'winvite':{},
+    }
+
+user1 = lineMID
+user2 = ""
+	
+setTime = {}
+setTime = rfuSet['setTime']
+
+contact = line.getProfile() 
+backup = line.getProfile() 
+backup.dispalyName = contact.displayName 
+backup.statusMessage = contact.statusMessage
+backup.pictureStatus = contact.pictureStatus
+
+mulai = time.time() 
+
+try:
+    with open("Log_data.json","r",encoding="utf_8_sig") as f:
+        msg_dict = json.loads(f.read())
+except:
+    print("Couldn't read Log data")
+
+myProfile["displayName"] = lineProfile.displayName
+myProfile["statusMessage"] = lineProfile.statusMessage
+myProfile["pictureStatus"] = lineProfile.pictureStatus
+#=========================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ด้านท้าย
+if op.type == 15:
+           print ("MEMBER LEAVE TO GROUP")
+           if settings["Lv"] == True:
+             if op.param2 in lineMID:
+                 return
+             dan = line.getContact(op.param2)
+             tgb = line.getGroup(op.param1)
+             line.sendMessage(op.param1,str(settings["bye"]))
+             line.sendContact(op.param1, op.param2)
+             line.sendImageWithURL(op.param1, "http://dl.profile.line-cdn.net{}".format(dan.picturePath))
+        if op.type == 55:
+            try:
+                if RfuCctv['cyduk'][op.param1]==True:
+                    if op.param1 in RfuCctv['point']:
+                        Name = line.getContact(op.param2).displayName
+                        if Name in RfuCctv['sidermem'][op.param1]:
+                            pass
+                        else:
+                            RfuCctv['sidermem'][op.param1] += "\n🔰" + Name
+                            pref=['จ๊ะเอ๋','รู้นะว่าแอบอยู่','เล่นซ่อนแอบกันเหรอ','คิดว่าเป็นนินจารึไง','ว่าไง','อ่านอย่างเดียวเลยนะ','ออกมาคุยหน่อย','ออกมาเดี๋ยวนี้']
+                            sendMessageWithMention(op.param1, op.param2)
+                            line.sendMessage(op.param1, str(random.choice(pref)) + '\n♪ ♬ ヾ(´︶`♡)ﾉ ♬ ♪')
+                            line.sendContact(op.param1, op.param2)
+                    else:
+                        pass
+                else:
+                    pass
+            except:
+                pass
+
+        if op.type == 55:
+            try:
+                if RfuCctv['cyduk'][op.param1]==True:
+                    if op.param1 in RfuCctv['point']:
+                        Name = line.getContact(op.param2).displayName
+                        if Name in RfuCctv['sidermem'][op.param1]:
+                            pass
+                        else:
+                            RfuCctv['sidermem'][op.param1] += "\n⌬ " + Name + "\n╚════════════════┛"
+                            if " " in Name:
+                            	nick = Name.split(' ')
+                            if len(nick) == 2:
+                            	line.sendMessage(op.param1, "Nah " +nick[0])
+                            summon(op.param1, [op.param2])
+                    else:
+                        pass
+                else:
+                    pass
+            except:
+                pass
+        if op.type == 55:
+            print ("[BY MIN HACK BOT]")
+            try:
+                if op.param1 in read['readPoint']:
+                    if op.param2 in read['readMember'][op.param1]:
+                        pass
+                    else:
+                        read['readMember'][op.param1] += op.param2
+                    read['ROM'][op.param1][op.param2] = op.param2
+                    backupData()
+                else:
+                   pass
+            except:
+                pass
+    except Exception as error:
+        logError(error)
+#==============================================================================#
+def a2():
+    now2 = datetime.now()
+    nowT = datetime.strftime(now2,"%M")
+    if nowT[14:] in ["10","20","30","40","50","00"]:
+        return False
+    else:
+        return True
+        
+
+while True:
+    try:
+        ops = oepoll.singleTrace(count=5)
+        if ops is not None:
+            for op in ops:
+                lineBot(op)
+                oepoll.setRevision(op.revision)
+    except Exception as e:
+        logError(e)
+
+def atend():
+    print("Saving")
+    with open("Log_data.json","w",encoding='utf8') as f:
+        json.dump(msg_dict, f, ensure_ascii=False, indent=4,separators=(',', ': '))
+    print("BYE")
+atexit.register(atend)
